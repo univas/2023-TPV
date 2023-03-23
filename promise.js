a291ff5bc11bc2d137cd3f9edffdcca86e82ffd6
Marcos Antônio dos Santos <marcaosi2014@gmail.com>
2023-03-23 19:04:51 -0300

    Aula 21-03

--- promise.js
+++ promise.js
@@ -12,17 +12,12 @@ var i = 1
 const promises = []
 console.log("Começou")
 for(let i = 1; i <= 10; i++){
-    const promise = executa(i, i*1000)
-        .then((val) => console.log(val))
-
+    const promise = executa(i, i*1000).then((val) => console.log(val))
     promises.push(promise)
 }
-
 Promise.all(promises).then(() => {
     console.log("Terminou")
 })
-
-
 function executa(num, timeout){
     return new Promise((resolve, reject) => {
         setTimeout(() => {
