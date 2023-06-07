66e35fa661d8146417aeaa018c2c768eaec9f395
Marcos Antonio dos Santos <marcosantonio@fuvs.br>
2023-06-06 21:34:47 -0300

    aula 06-06

--- revisao-react/src/App.js
+++ revisao-react/src/App.js
@@ -4,33 +4,59 @@ import { useEffect, useState } from 'react';
 import axios from 'axios'
 
 function App({title}) {
-  const [estado, setEstado] = useState("Valor Inicial")
-  const [nome, setNome] = useState("Marcos")
+  const [nome, setNome] = useState("")
   
   const [pessoas, setPessoas] = useState([])
 
-  useEffect(() => {
-    axios.get("http://localhost:3000/pessoa")
-    .then(res => {
-      setPessoas(res.data)
-    })
-    .catch(err => {
-      alert("API está indisponível")
-    })
-  }, [])
+  const [estaCarregando, setEstaCarregando] = useState(true)
 
-  
-  return (
+  useEffect(() => {
+    if(estaCarregando){
+      axios.get("http://localhost:3000/pessoa")
+      .then(res => {
+        setPessoas(res.data)
+        setEstaCarregando(false)
+      })
+      .catch(err => {
+        alert("API está indisponível")
+      })
+    }
+  }, [estaCarregando])
+
+  return estaCarregando ? (<h1 id='carregando'>Carregando</h1>) : (
     <>
+      <label>Nome</label>
+      <input onChange={(event) => {
+        const valor = event.target.value
+
+        setNome(valor)
+      }} value={nome}/>
       <ul>
         {
           pessoas.map((pessoa, index) => (
-            <li key={index}>{pessoa.nome}</li>
+            <li key={index} onClick={() => {
+              axios.get("http://localhost:3000/pessoa/"+pessoa.id)
+              .then((res) => {
+                alert(res.data.cargo)
+              }).catch(err => {
+                alert("não foi possível localizar")
+              })
+            }}>{pessoa.nome}</li>
           ))
         }
       </ul>
       <div>
-        <button onClick={() => {}}>Alterar Estado</button>
+        <button onClick={() => {
+          axios.post("http://localhost:3000/pessoa", {
+            nome, cargo: ""
+          }).then((qualquerOutraCoisa) => {
+            setNome("")
+            setEstaCarregando(true)
+            alert("salvo com sucesso")
+          }).catch((err) => {
+            alert("ocorreu um erro")
+          })
+        }}>Alterar Estado</button>
       </div>
     </>
   );
